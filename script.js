d8b11dcb2aa6685185505cc445842f2573fb2872
FATIMA QURESHI <115686917+Fatimaq07@users.noreply.github.com>
2025-08-12 01:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let prices = document.querySelectorAll(".prices");
+let prices = document.querySelectorAll('.prices');
 let total = 0;
-prices.forEach(price => total += parseFloat(price.textContent));
-let table = document.getElementById("groceryTable");
-let row = document.createElement("tr");
-let cell = document.createElement("td");
-cell.colSpan = 2;
-cell.textContent = "Total: " + total;
-row.appendChild(cell);
-table.appendChild(row);
+prices.forEach(price => {
+    total += parseFloat(price.textContent);
+});
+let table = document.getElementById('groceryTable');
+let newRow = document.createElement('tr');
+let newCell = document.createElement('td');
+newCell.colSpan = 2;
+newCell.textContent = 'Total Price: ' + total;
+newRow.appendChild(newCell);
+table.appendChild(newRow);
